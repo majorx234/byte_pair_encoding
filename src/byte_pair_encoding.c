7f20dfb14ba548d7c17e16a6f45b871693a0daa3
MajorX234 <majorx234@googlemail.com>
2025-03-16 00:14:56 +0100

    Save text in token, free dynamic allocated mem

--- src/byte_pair_encoding.c
+++ src/byte_pair_encoding.c
@@ -1,11 +1,13 @@
 #include "byte_pair_encoding.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
 
 typedef struct Pair {
-  char pair[2];
+  uint32_t l,r;
 } Pair;
 
 typedef struct Freq {
@@ -21,10 +23,15 @@ int compare_freqs(const void* a_raw, const void* b_raw) {
 
 char *byte_pair_encode(char *text) {
   Freq* freq_map = NULL;
-  hmdefault(freq_map, 0);
+  u_int32_t* vec_tokens = NULL;
   size_t text_size = strlen(text);
-  for (int i = 0; i < text_size - 1; i++) {
-    Pair pair = {.pair = {text[i],text[i+1]} };
+  for (int i = 0; i < text_size; i++) {
+    arrput(vec_tokens,text[i]);
+  }
+  for (int i = 0; i < arrlen(vec_tokens) - 1; i++) {
+    Pair pair = {.l = vec_tokens[i],
+                 .r = vec_tokens[i+1]
+    };
     ptrdiff_t index = hmgeti(freq_map, pair);
     if (index < 0) hmput(freq_map, pair, 1);
     else freq_map[index].value++;
@@ -42,8 +49,10 @@ char *byte_pair_encode(char *text) {
   qsort(vec_freq_sorted, vec_freq_sorted_length, sizeof(*vec_freq_sorted), compare_freqs);
 
   for (size_t i = 0; i < vec_freq_sorted_length; i++) {
-    printf("key: %.2s, value %d\n", vec_freq_sorted[i].key.pair, vec_freq_sorted[i].value);
+    printf("key: (%u,%u), value %lu\n", vec_freq_sorted[i].key.l,vec_freq_sorted[i].key.r , vec_freq_sorted[i].value);
   }
-
+  arrfree(vec_tokens);
+  arrfree(vec_freq_sorted);
+  hmfree(freq_map);
   // uses ds dynamic array
 }
