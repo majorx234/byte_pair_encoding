b6b969853cfd444150d253662a4b24ce734cf95f
MajorX234 <majorx234@googlemail.com>
2025-04-19 23:02:09 +0200

    Add stub for pairs 2 dot exe

---
+++ src/pairs2dot.c
@@ -0,0 +1,11 @@
+#include <string.h>
+#include <stdio.h>
+#include "byte_pair_encoding.h"
+
+int main(int argc, char **argv) {
+  if(argc !=2) {
+    printf("usage: pairs2dot /path/to/pairs /path/to/dot");
+  }
+  return 0;
+}
+
