8af605580b2da255b50a097d4a92324f38443bcd
MajorX234 <majorx234@googlemail.com>
2025-04-24 23:46:34 +0200

    Add basic thread infrastructure

--- src/byte_pair_encoding.c
+++ src/byte_pair_encoding.c
@@ -1,4 +1,5 @@
 #include "byte_pair_encoding.h"
+#include <bits/pthreadtypes.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -6,6 +7,7 @@
 #include <fcntl.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <pthread.h>
 
 #define STB_DS_IMPLEMENTATION
 #include "stb_ds.h"
@@ -106,6 +108,108 @@ void generate_dot(const char *file_path, Pair *pairs) {
 }
 
 char *byte_pair_encode(char *text) {
+  u_int32_t *vec_tokens_in = NULL;
+  size_t text_size = strlen(text);
+  Pair *pairs_lookup_table = NULL;
+  // generate basic lookup table
+  for (size_t i = 0; i < 256; i++) {
+    arrput(pairs_lookup_table, ((Pair){.l = i, .r = 0}));
+  }
+
+  for (int i = 0; i < text_size; i++) {
+    arrput(vec_tokens_in, text[i]);
+  }
+
+  int freq_map_length = 0;
+  while (arrlen(vec_tokens_in) > 1) {
+    Freq *freq_map = NULL;
+    for (int i = 0; i < arrlen(vec_tokens_in) - 1; i++) {
+      Pair pair = {.l = vec_tokens_in[i], .r = vec_tokens_in[i + 1]};
+      ptrdiff_t index = hmgeti(freq_map, pair);
+      if (index < 0)
+        hmput(freq_map, pair, 1);
+      else
+        freq_map[index].value++;
+    }
+
+    freq_map_length = hmlen(freq_map);
+    // find maximum
+    ptrdiff_t max_freq_index = 0;
+    for (size_t i = 1; i < freq_map_length; i++) {
+      if (freq_map[i].value > freq_map[max_freq_index].value) {
+        max_freq_index = i;
+      }
+    }
+    // check for end condition
+    if (freq_map[max_freq_index].value <= 1)
+      break;
+    // add pair with most appearance to pairlookuptable
+    arrput(pairs_lookup_table, freq_map[max_freq_index].key);
+    // adjust reorder tokens
+    uint32_t *vec_tokens_out = NULL;
+    for (size_t i = 0; i < arrlen(vec_tokens_in); i++) {
+      if (i + 1 > arrlen(vec_tokens_in)) {
+        arrput(vec_tokens_out, vec_tokens_in[i]);
+      } else {
+        Pair pair = {.l = vec_tokens_in[i], .r = vec_tokens_in[i + 1]};
+        if (memcmp(&pair, &freq_map[max_freq_index], sizeof(pair)) == 0) {
+          arrput(vec_tokens_out, arrlen(pairs_lookup_table) - 1);
+          i += 1; // skip next token
+        } else {
+          arrput(vec_tokens_out, vec_tokens_in[i]);
+        }
+      }
+    }
+    arrswap(&vec_tokens_in, &vec_tokens_out);
+    arrfree(vec_tokens_out);
+
+    hmfree(freq_map);
+  }
+
+  printf("byte pair encode finished with %d length of hashmap\n",
+         freq_map_length);
+  print_tokens(pairs_lookup_table, vec_tokens_in);
+
+  // print out
+  generate_dot("test.dot", pairs_lookup_table);
+  dump_pairs("test.dat", pairs_lookup_table);
+
+  arrfree(pairs_lookup_table);
+  arrfree(vec_tokens_in);
+  // uses ds dynamic array
+  return NULL;
+}
+
+#define THREAD_COUNT 8
+typedef struct ThreadStuff{
+  Freq* freqs[THREAD_COUNT];
+  pthread_cond_t collect_freqs;
+} ThreadStuff;
+
+typedef struct ThreadReturnStuff{
+  uint32_t ret;
+} ThreadReturnStuff;
+
+void* parallize_bpe() {
+  pthread_t thread_hdls[THREAD_COUNT];
+  for(size_t id = 0; id < THREAD_COUNT; ++id) {
+    int err = pthread_create(&thread_hdls[id], NULL, byte_pair_encode_threaded, NULL);
+    if (err != 0){
+      printf("cannot creat thread: %s\n", strerror(err));
+    }
+  }
+  // join threads
+  for(size_t id = 0; id < THREAD_COUNT; ++id) {
+    ThreadReturnStuff ret;
+    int err = pthread_join(thread_hdls[id], NULL);
+    if (err != 0){
+      printf("cannot creat thread: %s\n", strerror(err));
+    }
+  }
+}
+
+void *byte_pair_encode_threaded(void* thread_stuff) {
+  char *text;
   u_int32_t *vec_tokens_in = NULL;
   size_t text_size = strlen(text);
   Pair *pairs_lookup_table = NULL;
